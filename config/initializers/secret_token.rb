6fd2d8fe8b010168677b50c8f2e6aea58c654080
Stevan Litobac <stevan@litobac.com>
2014-01-06 22:37:24 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+W2Manliest::Application.config.secret_key_base = '8b76c21db2e7a8ce8cb9501b804d5a373ae5172e4005388e81d65e4f91cff09251a6090535f562b7eeae5fcdd3142e735ef9909cbf2d82b9f05eb97796f94cbb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+W2Manliest::Application.config.session_store :cookie_store, key: '_w2-manliest_session'
