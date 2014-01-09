65e0467fd29a42aeb30e1f965413672414e28258
Stevan Litobac <stevan@litobac.com>
2014-01-09 00:37:10 +0000

    [BUG] Add route to about us

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ W2Manliest::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  get 'about' => 'about#index', as: :about
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
